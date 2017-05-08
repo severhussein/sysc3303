3d6588f22ef52436d6d6893fd559aa88e5f08f57
cyang030 <clarkyang@cmail.carleton.ca>
2017-05-08 17:54:26 -0400

    Add print packet content wireshark style

--- src/Utils.java
+++ src/Utils.java
@@ -57,6 +57,70 @@ public class Utils {
 
 		return Arrays.copyOf(buf, i-1);
 	}
+	
+	public static void printDatagramContentWiresharkStyle(DatagramPacket packet) {
+
+		int len = packet.getLength();
+		byte payload[] = packet.getData();
+		int i = 0, count = 1;
+		boolean printHex = true, printHeader = true;
+
+		System.out.println("   This packet contains:");
+		System.out.println("   Address: " + packet.getAddress());
+		System.out.println("   Port: " + packet.getPort());
+		System.out.println("   Length: " + len);
+		System.out.print("   Content:");
+		while (i < payload.length) {
+			if (printHex) {
+				if (printHeader) {
+					System.out.println();
+					System.out.format("%04x | ", (i == 0) ? i : i + 1); //
+					printHeader = false;
+				}
+				System.out.print(String.format("%02x ", payload[i]));//
+				if (count == 16) {
+					printHex = false;
+					i -= 16;
+					count = 0;
+					System.out.print("   ");
+				} else if (count == 8) {
+					System.out.print("   ");
+				} else if (i + 1 == payload.length) {
+					// end of payload, switch to hex and compensate the space between hex/ASCII
+					printHex = false;
+					i -= count;
+					for (int j = 1; j <= (16 - count); j++) {
+						System.out.print("   ");
+					}
+					if (count <= 8) {
+						System.out.print("      ");
+					}
+					count = 1;
+				}
+			} else {
+				if (payload[i] == 0) {
+					System.out.print(".");//
+				} else {
+					System.out.print((char) payload[i]);// not quite, NEED TO
+														// make sure every
+														// platform supports the
+														// box with number
+				}
+				if (count == 16) {
+					printHex = true;
+					printHeader = true;
+					count = 0;
+				} else if (count == 8) {
+					System.out.print("   ");
+				}
+			}
+			i++;
+			count++;
+		}
+		System.out.println();
+		System.out.println();
+	}
+
 
 	/**
 	 * @return true if OS is Windows
