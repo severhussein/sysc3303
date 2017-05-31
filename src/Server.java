c9654fa440d2d76ca752406d021eb484d9266f7a
Yu-Kai Yang <clarkyang@cmail.carleton.ca>
2017-05-30 20:33:48 -0400

    Remove unused import.

--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,9 @@
-import java.net.DatagramSocket;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.InetAddress;
+import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
-import java.io.IOException;
-import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 public class Server {
